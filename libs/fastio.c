3ab18b2915e162f31efb7b8008e2f7953aafc4b0
Luengor <aluengor0001@gmail.com>
2023-04-03 13:01:51 +0200

    more fastio

--- libs/fastio.c
+++ libs/fastio.c
@@ -1,6 +1,16 @@
 #include "fastio.h"
-#include <stdio.h>
 
+/*
+ * Prints a CONSTANT string fast
+ */
+void fio_fast_print(const char *str, size_t str_len)
+{
+    fwrite_unlocked(str, 1, str_len, stdout); 
+}
+
+/*
+ * Reads a integer from stdin and consumes the next char
+ */
 int fio_parse_int()
 {
     register int n, c;
@@ -11,3 +21,26 @@ int fio_parse_int()
 
     return n;
 }
+
+/*
+ * Consumes n chars from stdin
+ */
+void fio_consume_nchars(int n)
+{
+    for (; n > 0; n--)
+        getchar_unlocked();
+}
+
+/*
+ * Consumes chars until c is found and returns the length
+ */
+size_t fio_consume_until(char c)
+{
+    size_t count = 0;
+    
+    while (getchar_unlocked() != c)
+        count++;
+
+    return count;
+}
+
--- libs/fastio.h
+++ libs/fastio.h
@@ -1,7 +1,13 @@
 #ifndef INCLUDE_FASTIO_H
 #define INCLUDE_FASTIO_H
 
+#include <stdio.h>
+
+void fio_fast_print(const char *, size_t s);
+
 int fio_parse_int();
+void fio_consume_nchars(int n);
+size_t fio_consume_until(char c);
 
 #endif
 
