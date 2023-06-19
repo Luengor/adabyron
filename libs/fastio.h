6e361148858df82a373d26e8d58ab761b50412dc
luengor <aluengor0001@gmail.com>
2023-06-19 20:07:56 +0200

    fastio got smaller

--- libs/fastio.h
+++ libs/fastio.h
@@ -9,40 +9,11 @@
  */
 #define fio_print(_str, _size)    \
     fwrite_unlocked(_str, 1, _size, stdout)
-
-#define fio_printd(_d)              \
-    for (; d > 0; d /= 10)          \
-        fputc_unlocked('0' % 10, stdout);
     
 /*
  * Reads an integer from stdin and consumes the next char
  */
 int fio_parse_int(void);
-
-/*
- * Reads an unsigned integer from stdin and consumes the next char
- */
-unsigned int fio_parse_uint(void);
-
-/*
- * Reads a long long from stdin and consumes the next char
- */
-long long fio_parse_longlong(void);
-
-/*
- * Reads a series of chars until end_char or maxn length
- */
-char *fio_parse_string(int maxn, char end_char);
-
-/*
- * Consumes n chars from stdin
- */
-void fio_consume_nchars(int n);
-
-/*
- * Consumes chars until c is found and returns the length
- */
-size_t fio_consume_until(char c);
 
 #endif
 
