8fd5f20fb350a1a6eb510e2003efd533bc7b8641
luengor <aluengor0001@gmail.com>
2023-06-19 20:56:00 +0200

    this is terrible
    mistake in fastio

--- libs/fastio.c
+++ libs/fastio.c
@@ -5,13 +5,22 @@
 int fio_parse_int(void)
 {
     int n, c;
+    int neg = 1;
 
-    n = getchar_unlocked() - '0';
-    if (n == ('-' - '0')) n = -1;
-
+    n = getchar_unlocked();
+    if (n == '-')
+    {
+        neg = -1;
+        n = 0;
+    }
+    else
+    {
+        n -= '0';
+    }
+    
     while ((c = getchar_unlocked()) >= '0')
         n = 10 * n + c - '0';
 
-    return n;
+    return n * neg;
 }
 
