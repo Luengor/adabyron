85180c2b644e3194db93e24b365d623342c73e3a
luengor <aluengor0001@gmail.com>
2025-06-25 19:48:09 +0200

    lets gooooooo

--- problems/v8/p810/main.cpp
+++ problems/v8/p810/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -7,59 +8,37 @@ typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
 typedef vector<ii> vii;
 
-// El problema realmente se basa en encontrar la subsecuencia decreciente seguida de una subsecuencia creciente más larga
-// longest decreasing subsequence (LDS) + longest increasing subsequence (LIS) = longest bitonic subsequence (LBS)
+#define INV(n) (-(n))
 
-// left_memo[i] contiene la longitud de la subsecuencia decreciente máxima que termina en el coche i
-vi left_memo;
-int left(int from, int to, const vi &pesos)
+vi get_lis(int n, const vi &A)
 {
-    if(from == to) return 1; // Si solo hay un coche, la subsecuencia decreciente es de longitud 1
-    auto &memo = left_memo[to];
-    if(memo != -1) return memo; // Si ya hemos calculado este subproblema, lo devolvemos
+    // k -> longitud de la major cadena
+    int k = 0;
+    // L -> Lista de la "secuencia" creciente más larga 
+    vi L(n, 0);
 
-    // Inicializamos la longitud de la subsecuencia decreciente
-    int max_length = 1;
+    // Longitud de la mejor secuencia hasta aquí
+    vi endings(n);
 
-    for (int i = to - 1; i >= from; i--)
+    for (int i = 0; i < n; ++i)
     {
-        // Si el coche que estamos viendo es más pesado que el último
-        if (pesos[i] > pesos[to])
-        {
-            // Obtenemos la subsecuencia decreciente máxima hasta ese coche y le añadimos este coche
-            int length = left(from, i, pesos) + 1; // +1 por el coche actual
-            max_length = max(max_length, length); // Actualizamos la longitud máxima
-        }
+        // Buscamos el primer valor en L mayor que el elemento actual
+        int pos = lower_bound(L.begin(), L.begin() + k, A[i]) - L.begin();
+
+        // La longitud de la secuencia es el índice que tenga en la secuencia más larga
+        endings[i] = pos + 1;
+
+        // Cambiamos ese valor en la secuencia más larga 
+        L[pos] = A[i];
+
+        // Si hemos añadido un elemento
+        if (pos == k)
+            // Aumentamos la longitud máxima
+            k = pos + 1;
     }
 
-    return memo = max_length;
+    return endings;
 }
-
-vi right_memo;
-int lis(int from, int to, const vi &pesos)
-{
-    if(from == to) return 1; // Si solo hay un coche, la subsecuencia creciente es de longitud 1
-    auto &memo = right_memo[from];
-    if(memo != -1) return memo; // Si ya hemos calculado este subproblema, lo devolvemos
-
-    // Inicializamos la longitud de la subsecuencia creciente
-    int max_length = 1; // La longitud mínima es 1 (el coche actual)
-
-    // Recorremos los coches desde 'from' hasta 'to'
-    for (int i = from + 1; i <= to; i++)
-    {
-        // Si el coche que estamos viendo es más pesado que el primero
-        if (pesos[i] > pesos[from])
-        {
-            // Obtenemos la subsecuencia creciente máxima a partir de ese coche y le añadimos este coche
-            int length = lis(i, to, pesos) + 1; // +1 por el coche actual
-            max_length = max(max_length, length); // Actualizamos la longitud máxima
-        }
-    }
-
-    return memo = max_length;
-}
-
 
 bool casoDePrueba()
 {
@@ -74,32 +53,40 @@ bool casoDePrueba()
         coches_pesos[i].second = i; // Guardamos el índice original para mantener el orden de llegada
     }
 
-
     // Ordenamos los coches por peso (de mayor a menor) y por índice (de menor a mayor) para mantener el orden de llegada
     sort(coches_pesos.begin(), coches_pesos.end());
 
     // Nos quedamos con los índices de los coches en el orden decreciente de peso
+    // Invertimos los índices ya que estamos
     vi indices(coches);
     for(int i = 0; i < coches; i++)
-        indices[i] = coches_pesos[i].second;
-
-    left_memo.assign(coches, -1);
-    right_memo.assign(coches, -1);
-
-    // Buscamos el punto medio con la mayor longitud de subsecuencia decreciente y creciente
-    int max_length = 0;
-    for (int i = 0; i < coches; i++)
     {
-        // Longitud de la subsecuencia decreciente desde el inicio hasta el coche i
-        int left_length = left(0, i, indices);
-        // Longitud de la subsecuencia creciente desde el coche i hasta el final
-        int right_length = lis(i, coches - 1, indices);
-
-        // Actualizamos la longitud máxima
-        max_length = max(max_length, left_length + right_length - 1); // -1 porque contamos el coche i dos veces
+        indices[i] = INV(coches_pesos[i].second);
+        // cout << indices[i] << ' ';
     }
+    // cout << endl;
 
-    cout << max_length << endl;
+    // Obtenemos la parte izquierda
+    vi izq = get_lis(coches, indices);
+    // for (auto v : izq)
+    //     cout << v << ' ';
+    // cout << endl;
+
+    // Reverseamos el vector para obtener la derecha
+    reverse(indices.begin(), indices.end());
+    vi der = get_lis(coches, indices);
+    reverse(der.begin(), der.end());
+
+    // for (auto v : der)
+    //     cout << v << ' ';
+    // cout << endl;
+
+    // Encontramos el máximo
+    int ans = 0;
+    for (int i = 0; i < coches; i++)
+        ans = max(ans, izq[i] + der[i] - 1);
+
+    cout << ans << '\n';
 
     return true;
 }
